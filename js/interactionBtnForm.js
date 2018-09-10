055625aa9d7cc2197eec4d225d5647bde25d45d2
jonatasb <jbonatoasato@gmail.com>
2018-09-10 08:05:00 -0400

    adicionando perdidos

--- js/interactionBtnForm.js
+++ js/interactionBtnForm.js
@@ -1,8 +1,33 @@
-var	formulary = document.querySelector("#formulary");
-
-document.getElementById('formulary').style.display = 'none';
-
 document.getElementById('form').onclick = function(){
+	document.querySelector('#cap3').innerHTML = 
+	`<section class="formu" id="formulary">
+
+		<div class="explication">
+			<h2>Chapter 3: now it's your turn!</h2>
+			<p>Make it worth it! Show what is capable to the world and be proactive. Studies reveal that when a person explains something to another person, it ends up refining their thinking and finding easier ways to arrive at a particular answer. The fixation of content for those who share their knowledge is 80% higher in comparison to those who keep things to themselves. Enjoy, free your knowledge!</p>
+		</div>
+		<div class="form" id="cadastro">
+			<form class="cadastro">
+				<h2>Register a word!</h2>
+				<div class="container-entrada">
+					<label for="word">Word:</label>
+					<input type="text" id="word">
+		
+				</div>
+
+				<div class="container-entrada">
+					<label for="example">Example:</label>
+					<input type="text" id="example">
+				</div>
+
+				<a id="cadastrarPalavra" href="#home">Register</a>
+			</form>
+		</div>
+
+	</section>`
+
+	var	formulary = document.querySelector("#formulary");
+
 	formulary.style.display = 'flex';
 
 	document.getElementById('formulary').style.height = '65vh';
--- js/interactionBtnForm2.js
+++ js/interactionBtnForm2.js
@@ -1,8 +1,28 @@
-var	search = document.querySelector("#search");
-
-document.getElementById('search').style.display = 'none';
-
 document.getElementById('form2').onclick = function(){
+	document.querySelector('#cap3').innerHTML = 
+	`<section class="procura" id="search">
+
+		<div class="explication">
+			<h2>Chapter 3: now it's your turn!</h2>
+			<p></p>
+		</div>
+
+		<div class="form" id="pesquisa">
+			<form class="cadastro">
+				<h2>Find a word!</h2>
+				<div class="container-entrada">
+					<label for="wordSearch">Word:</label>
+					<input type="text" id="wordSearch">
+				</div>
+
+				<a id="pesquisarPalavra" href="#cap3">Search</a>
+			</form>
+		</div>
+		
+	</section>`
+
+	var	search = document.querySelector("#search");
+
 	search.style.display = 'flex';
 
 	document.getElementById('search').style.height = '65vh';
